79b0bed114606db72f5aa30f9e1b8bb3047f1a57
Noor Sheikh <noor.sheikh@hobsons.com>
2017-06-27 20:21:38 -0400

    API for adding a new book (#5)
    * readme updated for technologies used
    
    * API for adding a book

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ ini_set("display_errors", 1);
 $rootDir = __DIR__;
 
 require_once $rootDir . "/vendor/autoload.php";
+require $rootDir . "/src/Domain/Book.php";
+require $rootDir . "/src/Domain/BookBuilder.php";
 require $rootDir . "/src/Repository/LibraryRepository.php";
 require $rootDir . "/src/Controller/LibraryController.php";
 
