c62a4bd9c7b20ffb5b670451b6f0bcf617760637
Noor Sheikh <noor.sheikh@hobsons.com>
2017-07-01 01:02:47 -0400

    autoloading and namespace fix (#9)

--- index.php
+++ index.php
@@ -6,18 +6,6 @@ $rootDir = __DIR__;
 
 require_once $rootDir . "/vendor/autoload.php";
 
-require $rootDir . "/src/Domain/Book.php";
-require $rootDir . "/src/Domain/BookBuilder.php";
-require $rootDir . "/src/Domain/Copy.php";
-require $rootDir . "/src/Domain/CopyBuilder.php";
-require $rootDir . "/src/Domain/Borrower.php";
-require $rootDir . "/src/Domain/BorrowerBuilder.php";
-
-require $rootDir . "/src/Persistence/Repository.php";
-require $rootDir . "/src/Persistence/LibraryRepository.php";
-
-require $rootDir . "/src/Controller/LibraryController.php";
-
 $app = require_once $rootDir . "/app/app.php";
 
 $app->run();
