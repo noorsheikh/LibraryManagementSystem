6dab649d02873a028653197f2fbd4e325cc0770a
Noor Sheikh <noorsheikh.me@gmail.com>
2017-10-25 15:21:28 -0400

    unit tests added for repository

--- src/test/php/Library/Persistence/LibraryRepositoryTest.php
+++ src/test/php/Library/Persistence/LibraryRepositoryTest.php
@@ -4,15 +4,22 @@ namespace Library\Persistence;
 
 use Silex\WebTestCase;
 
-use Library\Domain\Book;
 use Library\Domain\BookBuilder;
+use Library\Domain\CopyBuilder;
+use Library\Domain\BorrowerBuilder;
 
 class LibraryRepositoryTest extends WebTestCase
 {
     const BASE_DIR = __DIR__;
+    const BOOK_ID = 1;
     const TITLE = 'title';
     const AUTHOR = 'author';
     const ISBN_NUMBER = '123456';
+    const COPY_NUMBER = 2;
+    const BORROWER_ID = 1;
+    const BORROWER_NAME = 'noor';
+    const MEMBERSHIP_ID = 12345;
+    const MEMBERSHIP_STATUS = 1;
 
     public function createApplication()
     {
@@ -22,7 +29,7 @@ class LibraryRepositoryTest extends WebTestCase
     /**
      * @test
      */
-    public function itShouldAddNewBook()
+    public function itShouldAddANewBook()
     {
         $connection = $this->getConnectionMock();
         $repository = new LibraryRepository($connection);
@@ -40,6 +47,117 @@ class LibraryRepositoryTest extends WebTestCase
         $repository->addBook($book);
     }
 
+    /**
+     * @test
+     */
+    public function itShouldReturnAllBooks()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $connection
+            ->expects($this->once())
+            ->method("prepare")
+            ->will($this->returnValue($this->getStatementMock()));
+
+        $repository->getBooks();
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnASingleBook()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $connection
+            ->expects($this->once())
+            ->method("prepare")
+            ->will($this->returnValue($this->getStatementMock()));
+
+        $repository->getBook(self::BOOK_ID);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldListAvailableBooks()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $connection
+            ->expects($this->once())
+            ->method("prepare")
+            ->will($this->returnValue($this->getStatementMock()));
+
+        $repository->listAvailableBooks();
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldAddACopyForBook()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $copy = CopyBuilder::instance()
+            ->withIsbnNumber(self::ISBN_NUMBER)
+            ->withCopyNumber(self::COPY_NUMBER)
+            ->withBookId(self::BOOK_ID)
+            ->build();
+
+        $connection
+            ->expects($this->once())
+            ->method("insert");
+
+        $repository->addCopy($copy);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldAddBorrower()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $borrower = BorrowerBuilder::instance()
+            ->withBorrowerName(self::BORROWER_NAME)
+            ->withBorrowerMembershipId(self::MEMBERSHIP_ID)
+            ->withStatus(self::MEMBERSHIP_STATUS)
+            ->build();
+
+        $connection
+            ->expects($this->once())
+            ->method("insert");
+
+        $repository->addBorrower($borrower);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldUpdateAnExistingBorrower()
+    {
+        $connection = $this->getConnectionMock();
+        $repository = new LibraryRepository($connection);
+
+        $borrower = BorrowerBuilder::instance()
+            ->withBorrowerName(self::BORROWER_NAME)
+            ->withBorrowerMembershipId(self::MEMBERSHIP_ID)
+            ->withStatus(self::MEMBERSHIP_STATUS)
+            ->build();
+
+        $connection
+            ->expects($this->once())
+            ->method("update");
+
+        $repository->updateBorrower($borrower, self::BORROWER_ID);
+    }
+
     private function getConnectionMock()
     {
         $connectionMock = $this->getMockBuilder("\Doctrine\DBAL\Connection")
@@ -47,5 +165,14 @@ class LibraryRepositoryTest extends WebTestCase
             ->getMock();
 
         return $connectionMock;
+    }
+
+    private function getStatementMock()
+    {
+        $statementMock = $this->getMockBuilder("\Doctrine\DBAL\Driver\Statement")
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $statementMock;
     }
 }
